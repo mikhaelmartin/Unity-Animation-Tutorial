f19edb49abe2002e651b96fc99b2b6e37759aae9
Mikhael Martin <mikhael.martin@gmail.com>
2022-10-13 23:53:49 +0700

    Animation Event Test

---
+++ Assets/AnimationEventTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventTest : MonoBehaviour
+{
+    public void DebugMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
